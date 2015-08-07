cc279b492ca3d5e579824c3968e376cfdbca537e
David Liebman <radiodee1@gmail.com>
2015-08-07 10:23:14 -0400

    multiple characters one at a time.

--- cnn_mnist_show.py
+++ cnn_mnist_show.py
@@ -11,7 +11,7 @@ import math, sys
 from nnet.helpers import one_hot, unhot
 
 def run():
-    #conv.conv.print_test()
+    '''
     # Fetch data
     mnist = sklearn.datasets.fetch_mldata('MNIST original', data_home='./data')
     split = 60000
@@ -27,6 +27,9 @@ def run():
     #train_idxs = np.array([i for i in range(n_train_samples)])
     X_train = X_train[train_idxs, ...]
     y_train = y_train[train_idxs, ...]
+    '''
+    X_train, y_train = fetch_mnist_img()
+    n_classes = 10
     name = "mnist"
 
     # Setup convolutional neural network
@@ -63,26 +66,30 @@ def run():
         ],
     )
 
-    
+    done_looping = True
+    while done_looping:
+        X_train, y_train = fetch_mnist_img()
 
-    Y_one_hot = numeric_ymatrix(y_train[0], n_classes) 
-    Y_one_hot = np.array(Y_one_hot)
-    
-    X = X_train[0][0]
-    X = np.reshape(X,(-1,1,28,28))
-    Y = np.reshape(Y_one_hot,(1,10))
-    
-    nn._setup(X, Y)
-    nn.load_file(name=name)
-    
-    ## the following two lines are just for textual display
-    X_disp = shape_x(X_train[0][0])
-    show_xvalues([X_disp], index=0)
-    
-    print "stored value: " + str( int(y_train[0]))
-    print Y
-    print("prediction: " + str( nn.predict(X)[0]))
-    
+        Y_one_hot = numeric_ymatrix(y_train[0], n_classes) 
+        Y_one_hot = np.array(Y_one_hot)
+        Y = np.reshape(Y_one_hot,(1,10))
+        
+        X = X_train[0][0]
+        X = np.reshape(X,(-1,1,28,28))
+        
+        nn._setup(X, Y)
+        nn.load_file(name=name)
+        
+        ## the following two lines are just for textual display
+        X_disp = shape_x(X_train[0][0])
+        show_xvalues([X_disp], index=0)
+        
+        print "stored value: " + str( int(y_train[0]))
+        print Y
+        print("prediction: " + str( nn.predict(X)[0]))
+        
+        xx = raw_input("more? (Y/n): ")
+        if xx.strip() == 'n' or xx.strip() == 'N' : done_looping = False
 
 
 def shape_x(x):
@@ -119,6 +126,25 @@ def show_xvalues(xarray = [[]], index = 0):
             sys.stdout.write(zzz)
         print("");
     print ("\n===========================\n")
+
+def fetch_mnist_img():
+    #conv.conv.print_test()
+    # Fetch data
+    mnist = sklearn.datasets.fetch_mldata('MNIST original', data_home='./data')
+    split = 60000
+    X_train = np.reshape(mnist.data[:split], (-1, 1, 28, 28))/255.0
+    y_train = mnist.target[:split]
+    X_test = np.reshape(mnist.data[split:], (-1, 1, 28, 28))/255.0
+    y_test = mnist.target[split:]
+    n_classes = np.unique(y_train).size
+
+    # Downsample training data
+    n_train_samples = 1 #3000
+    train_idxs = np.random.random_integers(0, split-1, n_train_samples)
+    #train_idxs = np.array([i for i in range(n_train_samples)])
+    X_train = X_train[train_idxs, ...]
+    y_train = y_train[train_idxs, ...]
+    return X_train, y_train
 
 if __name__ == '__main__':
     run()
